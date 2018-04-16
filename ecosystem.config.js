a3e04ea63c55c0439b0e7653c847e9f07626a299
milad rezazadeh <milad.rk@live.com>
2018-04-16 11:24:23 +0200

    re-model build folder structure, enhance gulp build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,13 +2,8 @@ module.exports = {
   apps : [
     {
       name      : 'VSMA-API',
-      script    : 'NODE_ENV=production node ./dist/bin/www',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
+      script    : 'npm',
+      args      : 'start'
     }
   ]
 };
--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@ const uglify = require('gulp-uglify');
 const ts = require('gulp-typescript');
 const tslint = require("gulp-tslint");
 
-
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('tslint', () =>
@@ -17,14 +16,27 @@ gulp.task('tslint', () =>
         }))
 );
 
-gulp.task('build', [], () => {
+gulp.task('copy-files', [], () => {
+
+    gulp.src('config/*.json')
+        .pipe(gulp.dest('build/config'));
+
+    return gulp.src(
+        [
+            'package.json',
+            'ecosystem.config.js'
+        ])
+        .pipe(gulp.dest('build'));
+});
+
+gulp.task('build', ['copy-files'], () => {
 
     return tsProject.src()
         .pipe(ts())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('build/dist'));
 });
 
-gulp.task('watch', ['tslint', 'build'], () => {
+gulp.task('watch', ['build'], () => {
     gulp.watch('src/**/*.ts', ['tslint', 'build']);
 });
 
