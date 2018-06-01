34929a930093faf6875debda90b62b056dfa22e7
milad rezazadeh <milad.rk@live.com>
2018-06-01 13:29:37 +0200

    add response model, and response codes enum

---
+++ src/common/response.model.ts
@@ -0,0 +1,37 @@
+import {ResponseErrorCode} from './responseCodes.enum';
+
+class ResponseClass {
+
+}
+
+export class SuccessResponse extends ResponseClass {
+
+    public data: any;
+
+    constructor(
+        data: any
+    ) {
+        super();
+
+        this.data = data;
+    };
+
+}
+
+export class ErrorResponse extends ResponseClass {
+
+    public error: object;
+
+    constructor(
+        code: ResponseErrorCode,
+        message: string
+    ) {
+
+        super();
+
+        this.error =  {
+            code: code,
+            message: message
+        };
+    };
+}
---
+++ src/common/responseCodes.enum.ts
@@ -0,0 +1,5 @@
+export enum ResponseErrorCode {
+    NotFound = "NotFound",
+    BadRequest = "BadRequest",
+    Unauthorized = "Unauthorized"
+}
