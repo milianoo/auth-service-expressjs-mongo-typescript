f50c1ffa4d57fdf75c2479aac8d309c20a114872
milad rezazadeh <milad.rk@live.com>
2018-06-01 13:51:50 +0200

    enhance app start file

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,23 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
 import * as bodyParser from 'body-parser';
-import * as passport from 'passport';
 import * as cors from 'cors';
 import * as helmet from 'helmet';
+import * as frameguard from 'frameguard';
+import * as swaggerUi from 'swagger-ui-express';
 
-import { MongoDb } from './bin/mongodb';
-import { AuthMiddleware } from './auth/auth.middleware';
+let swaggerDocument = require('./swagger.json');
 
-import { AuthRouter } from './auth/auth.router';
-import { UserRouter } from './users/user.router';
-import { QuestionsRouter } from './survey/questions/questions.router';
-import { CategoryRouter } from './survey/category/category.router';
-import { SurveyRouter } from './survey/survey.router';
-import { CompanyRouter } from './company/company.router';
 import * as config from 'config';
 import * as debug from 'debug';
 const log = debug('api:express');
+import logger from './logger';
+
+import { MongoDb } from './bin/mongodb';
+import { Redis } from './bin/redis';
+import { auth } from './auth/auth.middleware';
+import { AuthRouter } from './auth/auth.router';
+import { UserRouter } from './users/user.router';
 
 class App {
 
@@ -27,17 +28,24 @@ class App {
         // setup connections
         MongoDb.connect();
 
+        log(`Redis database is ${ Redis.enabled() ? 'enabled' : 'disabled'}.`);
+        if (Redis.enabled()) {
+            Redis.connect();
+        }
+
         // setup express app
         this.express = express();
         this.middleware();
         this.routes();
+        this.setErrorHandlers();
         log(`express setup completed.`);
     }
 
     // Configure Express middleware.
     private middleware(): void {
         this.express.use(helmet());
-        this.express.use(morgan('combined'));
+        this.express.use(frameguard({ action: 'deny' }));
+        this.express.use(morgan('combined', { 'stream': logger.stream }));
         this.express.use(bodyParser.json());
         this.express.use(bodyParser.urlencoded({ extended: false }));
         this.express.use(cors({
@@ -51,34 +59,47 @@ class App {
                 return next(null, true);
             }
         }));
-        this.express.use(passport.initialize());
-        AuthMiddleware.setup(passport);
+        this.express.use(auth.initialize());
     }
 
     private routes(): void {
 
+        this.express.use('/meta', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
         let auth = new AuthRouter().router;
         let user = new UserRouter().router;
-        let company = new CompanyRouter().router;
-        let questions = new QuestionsRouter().router;
-        let category = new CategoryRouter().router;
-        let survey = new SurveyRouter().router;
 
         let enabledRoutes = [
             auth,
-            user,
-            company,
-            questions,
-            category,
-            survey
+            user
         ];
 
         this.express.use('/api', enabledRoutes);
 
+        // to log the available paths
         // enabledRoutes.forEach(router =>
         //     router.stack.forEach(layer => {
         //         console.log(`${layer.route.path}  \n\t ${layer.route.stack.map(l => l.name === 'authenticate' ? '(auth)' : `${l.method} | `).join(' ')} \n`);
         //     }));
+    }
+
+    private setErrorHandlers(): void {
+
+        this.express.use((err, req, res, next) => {
+            // set locals, only providing error in development
+            res.locals.message = err.message;
+            res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+            logger.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+
+            // render the error page
+            res.status(err.status || 500);
+            res.status(404).send({ message: `unexpected error: ${err.message}`});
+        });
+
+        this.express.use((req, res, next) => {
+            res.status(404).send({ message: 'resource not available.'});
+        });
     }
 }
 
